5d3bb026f54065bf129c27f80920dc504e587ef5
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 22:32:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,8 @@ require "yaml"
 
 def load_library(file_path)
   emoticon_hash => {
-    get_meaning => {}
+    get_meaning => {},
+    get_emoticon =>
   }
 
   emoticon_lang = YAML.load_file(file_path)
